49004ef94ba07f3247c365ec2d05124791e86624
kc0817 <kevinjkchen17@gmail.com>
2025-09-07 18:28:35 -0400

    color sensor testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MultiThreadingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MultiThreadingTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import android.graphics.Color;
+
 import androidx.annotation.NonNull;
 
 import com.bylazar.configurables.annotations.Configurable;
@@ -12,45 +14,51 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 public class MultiThreadingTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-//        telemetry.setMsTransmissionInterval(11);
-//
-//        ColorSensor c1 = hardwareMap.get(ColorSensor.class, "c1");
-//        ColorSensor c2 = hardwareMap.get(ColorSensor.class, "c2");
-//        ColorSensor c3 = hardwareMap.get(ColorSensor.class, "c3");
-//
-////        Thread thread2 = new Thread() {
-////            @Override
-////            public void run() {
-////                while(opModeIsActive()) {
-////                    double prevTime = System.currentTimeMillis();
-////                    if (gamepad1.a)
-////                        telemetry.addData("c1", c1.red() + ", " + c1.green() + ", " + c1.blue());
-////                    if (gamepad1.b)
-////                        telemetry.addData("c2", c2.red() + ", " + c2.green() + ", " + c2.blue());
-////                    if (gamepad1.y)
-////                        telemetry.addData("c3", c3.red() + ", " + c3.green() + ", " + c3.blue());
-////                    telemetry.addData("thread dt", (System.currentTimeMillis() - prevTime)/1000.);
-////                }
-////            }
-////        };
-//
-//        waitForStart();
-////        thread2.start();
-//        while(opModeIsActive()) {
-//            double t1 = System.currentTimeMillis();
-//
-////            if(gamepad1.a)
-////                telemetry.addData("c1", c1.red() + ", " + c1.green() + ", " + c1.blue());
-////            if(gamepad1.b)
-////                telemetry.addData("c2", c2.red() + ", " + c2.green() + ", " + c2.blue());
-////            if(gamepad1.y)
-////                telemetry.addData("c3", c3.red() + ", " + c3.green() + ", " + c3.blue());
-//
-//            double t2 = System.currentTimeMillis();
-//            double dt = (t2 - t1) / 1000.0;
-//
-//            telemetry.addData("main fps", 1/dt);
-//            telemetry.update();
-//        }
+        telemetry.setMsTransmissionInterval(11);
+        boolean mode = true;
+
+        ColorSensor c1 = hardwareMap.get(ColorSensor.class, "c1");
+        ColorSensor c2 = hardwareMap.get(ColorSensor.class, "c2");
+        ColorSensor c3 = hardwareMap.get(ColorSensor.class, "c3");
+
+
+
+        waitForStart();
+        while(opModeIsActive()) {
+            double t1 = System.currentTimeMillis();
+            mode = gamepad1.x;
+            telemetry.addData("mode", mode);
+            if(mode) {
+                if (gamepad1.a) {
+                    telemetry.addData("c1", c1.red() + ", " + c1.green() + ", " + c1.blue());
+                }
+                if (gamepad1.b) {
+                    telemetry.addData("c2", c2.red() + ", " + c2.green() + ", " + c2.blue());
+                }
+                if (gamepad1.y) {
+                    telemetry.addData("c3", c3.red() + ", " + c3.green() + ", " + c3.blue());
+                }
+            }
+            else {
+                if (gamepad1.a) {
+                    int color = c1.argb();
+                    telemetry.addData("c1", Color.red(color) + ", " + Color.green(color) + ", " + Color.blue(color));
+                }
+                if (gamepad1.b) {
+                    int color = c2.argb();
+                    telemetry.addData("c2", Color.red(color) + ", " + Color.green(color) + ", " + Color.blue(color));
+                }
+                if (gamepad1.y) {
+                    int color = c3.argb();
+                    telemetry.addData("c3", Color.red(color) + ", " + Color.green(color) + ", " + Color.blue(color));
+                }
+            }
+
+            double t2 = System.currentTimeMillis();
+            double dt = (t2 - t1) / 1000.0;
+
+            telemetry.addData("main fps", 1/dt);
+            telemetry.update();
+        }
     }
 }
