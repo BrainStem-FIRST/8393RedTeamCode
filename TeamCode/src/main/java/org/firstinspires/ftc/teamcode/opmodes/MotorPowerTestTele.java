f1dabc1a9e2ad90ac410eb5274d0391f1167d487
Justin16Chen <justin.chen0160@gmail.com>
2025-09-07 14:38:51 -0400

    stuff we will need in future
    motor power test tele for testing shooter and intake and transfer
    PID is for indexer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MotorPowerTestTele.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Configurable
+@TeleOp(name="Motor power test tele")
+public class MotorPowerTestTele extends LinearOpMode {
+    public static String motorName = "BR";
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, motorName);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        waitForStart();
+        while(opModeIsActive()) {
+            motor.setPower(-gamepad1.right_stick_y);
+
+            telemetry.addData("gamepad1 right y", gamepad1.right_stick_y);
+            telemetry.addData("power", motor.getPower());
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/PIDController.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode.utils;
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.util.Range;
+public class PIDController {
+    protected double target;
+    protected double kP, kI, kD;
+    protected double proportional, integral, derivative;
+    protected boolean shouldReset;
+
+    protected double previousTime, previousError;
+
+    protected double lowerInputBound = Double.NEGATIVE_INFINITY, higherInputBound = Double.POSITIVE_INFINITY;
+    protected double lowerOutputBound = Double.NEGATIVE_INFINITY, higherOutputBound = Double.POSITIVE_INFINITY;
+
+    public PIDController(double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+        shouldReset = true;
+    }
+
+    public void setInputBounds(double lowerInputBound, double higherInputBound) {
+        this.lowerInputBound = lowerInputBound;
+        this.higherInputBound = higherInputBound;
+    }
+
+    public void setOutputBounds(double lowerOutputBound, double higherOutputBound) {
+        this.lowerOutputBound = lowerOutputBound;
+        this.higherOutputBound = higherOutputBound;
+    }
+
+
+    // returns motor power given current position of motor
+    public double update(double value) {
+        value = Range.clip(value, lowerInputBound, higherInputBound);
+
+        // BEFORE: double error = value - target;
+        double error = target - value;
+
+        return updateWithError(error);
+    }
+
+    private double updateWithError(double error) {
+        if (Double.isNaN(error) || Double.isInfinite(error))
+            return 0;
+
+        proportional = kP * error;
+
+        double currentTime = System.currentTimeMillis() / 1000.0;
+
+        if (shouldReset) {
+            shouldReset = false;
+            integral = 0;
+            derivative = 0;
+        }
+        else {
+            double dT = currentTime - previousTime;
+
+            integral += kI * error * dT;
+
+            derivative = kD * (error - previousError) / dT;
+        }
+
+        previousTime = currentTime;
+        previousError = error;
+
+        double correction = proportional + integral + derivative;
+
+        return Math.signum(correction) * Range.clip(Math.abs(correction),
+                lowerOutputBound, higherOutputBound);
+    }
+
+
+    // getters/setters
+    public double getKP() {
+        return kP;
+    }
+    public double getKI() {
+        return kI;
+    }
+    public double getKD() {
+        return kD;
+    }
+    // the target position of motor
+    public double getTarget() {
+        return target;
+    }
+    public void setTarget(double target) {
+        shouldReset = true;
+        this.target = target;
+    }
+    @NonNull
+    public String toString() {
+        return "kP:" + kP + " | kI: " + kI + " | kD: " + kD;
+    }
+}
