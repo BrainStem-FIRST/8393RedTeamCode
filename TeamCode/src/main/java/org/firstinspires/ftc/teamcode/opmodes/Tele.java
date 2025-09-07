fb025d7e5a36e1d610a3f993423023d1aefcc7d6
Justin16Chen <justin.chen0160@gmail.com>
2025-09-07 14:03:42 -0400

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="tele")
+public class Tele extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        while(opModeIsActive()) {
+
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.utils.GamepadTracker;
+
+public class Robot {
+    public final HardwareMap hardwareMap;
+    public final Telemetry telemetry;
+    public final GamepadTracker g1, g2;
+
+    public Robot(HardwareMap hardwareMap, Telemetry telemetry, GamepadTracker g1, GamepadTracker g2) {
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+        this.g1 = g1;
+        this.g2 = g2;
+    }
+}
