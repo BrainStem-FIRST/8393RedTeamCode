ad652d9a3f3c245379a9a2ea362c9a68bdefdce3
kc0817 <kevinjkchen17@gmail.com>
2025-09-11 08:33:04 -0400

    prepping for pedro path tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele.java
@@ -3,13 +3,19 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.robot.Robot;
+import org.firstinspires.ftc.teamcode.utils.GamepadTracker;
+
 @TeleOp(name="tele")
 public class Tele extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        Robot robot = new Robot(hardwareMap, telemetry, new GamepadTracker(gamepad1), new GamepadTracker(gamepad2), true);
+
         waitForStart();
         while(opModeIsActive()) {
-
+            robot.update();
+            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -1,21 +1,36 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.utils.GamepadTracker;
 
 public class Robot {
     public final HardwareMap hardwareMap;
     public final Telemetry telemetry;
     public final GamepadTracker g1, g2;
+    public final Follower follower;
+    public final IntakeIndexer intakeIndexer;
 
-    public Robot(HardwareMap hardwareMap, Telemetry telemetry, GamepadTracker g1, GamepadTracker g2) {
+    public Robot(HardwareMap hardwareMap, Telemetry telemetry, GamepadTracker g1, GamepadTracker g2, boolean teleop) {
         this.hardwareMap = hardwareMap;
         this.telemetry = telemetry;
         this.g1 = g1;
         this.g2 = g2;
+
+        intakeIndexer = new IntakeIndexer(this);
+
+        follower = Constants.createFollower(hardwareMap);
+        if(teleop)
+            follower.startTeleopDrive();
+    }
+    public void update() {
+        intakeIndexer.update();
+        follower.update();
     }
 }
